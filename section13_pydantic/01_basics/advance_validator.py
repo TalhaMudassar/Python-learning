f01d90fda104ba2023d055115686f7d20a43e205
TalhaMudassar <talhamudassar200@gmail.com>
2025-10-28 11:34:13 +0500

    add new code files

--- section13_pydantic/01_basics/advance_validator.py
+++ section13_pydantic/01_basics/advance_validator.py
@@ -1,38 +1,41 @@
-from pydantic import BaseModel,field_validator,model_validator
+from pydantic import BaseModel, field_validator, model_validator
 from datetime import datetime
+
 class Person(BaseModel):
-    first_name : str
-    last_name : str
-    
-    @field_validator('first_name','last_name')
-    def names_must_be_capatilize(cls,v):
+    first_name: str
+    last_name: str
+
+    @field_validator('first_name', 'last_name')
+    def names_must_be_capitalized(cls, v):
         if not v.istitle():
-            raise ValueError("Names must be capatilized")
+            raise ValueError("Names must be capitalized")
         return v
-    
+
+
 class User(BaseModel):
-    email : str
-    
+    email: str
+
     @field_validator('email')
-    def normalize_email(cls,v):
+    def normalize_email(cls, v):
         return v.lower().strip()
-    
+
+
 class Product(BaseModel):
-    price : str #$4.44
-    
-    @field_validator('price',mode='before')
-    def parse_price(cls,v):
-        if isinstance(v,str):
-            return float(v.replace('%','').replace(',',''))
-        
-        
-class Daterange(BaseModel):
-    start_date : datetime
-    end_date : datetime
-    
+    price: float  # e.g., "$4.44"
+
+    @field_validator('price', mode='before')
+    def parse_price(cls, v):
+        if isinstance(v, str):
+            return float(v.replace('$', '').replace(',', '').strip())
+        return v
+
+
+class DateRange(BaseModel):
+    start_date: datetime
+    end_date: datetime
+
     @model_validator(mode="after")
-    def validate_date_range(cls,values):
-        if values.start_date >= values.end_date:
-            raise ValueError ('end_date  must be after the start_date')
-        return values
-    
+    def validate_date_range(self):
+        if self.start_date >= self.end_date:
+            raise ValueError('end_date must be after start_date')
+        return self
