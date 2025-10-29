2e17ed7ea3867ec6a9e9a21ca229e7eedce22a63
TalhaMudassar <talhamudassar200@gmail.com>
2025-10-29 23:08:06 +0500

    add new files

---
+++ section2 datatypes/practice lists/sum_items.py
@@ -0,0 +1,13 @@
+list1 = [1,2,3,4,5,6,7]
+
+
+def sum(list1):
+    sum = 0
+    for i in list1:
+        sum += list1[i]
+    return sum
+
+print(f"the sum of list elements are {sum(list1)}")
+        
+    
+    
