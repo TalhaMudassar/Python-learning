cb61131c032d26d2fd61437ea1ae5318d709c42e
TalhaMudassar <talhamudassar200@gmail.com>
2025-11-02 22:37:18 +0500

    add new codes

---
+++ section2 datatypes/practice_lists/13_generate_3d_array.py
@@ -0,0 +1,9 @@
+# Write a Python program to generate a 3*4*6 3D array whose each element is 
+
+array = [
+    [['*' for _ in range(6)]
+     for _ in range(4)]
+     for _ in range(3) 
+    
+        ]
+print(array)
