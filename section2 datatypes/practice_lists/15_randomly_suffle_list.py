2cf07735a2f8b329753e179a97f249b329a86c5b
TalhaMudassar <talhamudassar200@gmail.com>
2025-11-04 23:16:04 +0500

    add new code files

--- section2 datatypes/practice_lists/15_randomly_suffle_list.py
+++ section2 datatypes/practice_lists/15_randomly_suffle_list.py
@@ -1,4 +1,4 @@
-from random import shuffle
+from random import shuffle # use shuffle    
 
 color = ["red","green","blue","black","yellow","orange","purple"]
 
--- section2 datatypes/practice_lists/16_checkprimeno.py
+++ section2 datatypes/practice_lists/16_checkprimeno.py
@@ -1,27 +1,19 @@
-from collections import Counter
+list_1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
-cipher_text = "SDS CXOFOB DOY VKDO DOY LO GRKD IYE WSQRD RKFO LOOX"
-
-# Step 1: Count frequency of letters
-letters_only = [c for c in cipher_text if c.isalpha()]
-freq = Counter(letters_only)
-most_common_letter = freq.most_common(1)[0][0]
-
-# Step 2: Assume most frequent ciphertext letter = 'E'
-key = (ord(most_common_letter) - ord('E')) % 26
-print("Most frequent letter:", most_common_letter)
-print("Estimated key:", key)
-
-# Step 3: Decrypt using that key
-def decrypt_caesar(text, shift):
-    result = ""
-    for char in text:
-        if char.isalpha():
-            offset = 65 if char.isupper() else 97
-            result += chr((ord(char) - offset - shift) % 26 + offset)
-        else:
-            result += char
-    return result
-
-plaintext = decrypt_caesar(cipher_text, key)
-print("Decrypted text:", plaintext)
+for i in list_1:
+    if i<=1:
+        continue
+    flag = True
+    
+    for j in range(2,i):
+        if i%j == 0:
+            flag = False
+            break
+        
+    if flag:
+        print(f"{i} is a prime Number") 
+        
+            
+            
+    
+    
